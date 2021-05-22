8dd984a107338731bc93385fd0c0c6f4967ed9db
yuvalBerghaus <markberghaus@outlook.com>
2021-05-23 00:19:53 +0300

    static definition was initialized outside of the class

--- patternsTraining/singleTon/singleTon/Services.cpp
+++ patternsTraining/singleTon/singleTon/Services.cpp
@@ -1,5 +1,6 @@
 #include "Services.h"
-
+#include <iostream>
+using namespace std;
 int Services::getAge()
 {
 	return age;
@@ -23,6 +24,7 @@ void Services::setLvl(char m_level)
 Services* Services::getInstance(int m_age,char m_level) {
 	if (obj == nullptr) {
 		Services::obj = (new Services(m_age,m_level));
+		cout << "the object was created first time!";
 	}
 	return Services::obj;
 };
@@ -32,4 +34,4 @@ Services::Services(int m_age, char m_level) {
 	level = m_level;
 };
 
-Services* Services::obj;
+Services* Services::obj = nullptr;
